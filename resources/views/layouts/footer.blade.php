afe47c509abe38a7f89e90872f59687de647c14b
gurin-evgeniy <74146767+gurin-evgeniy@users.noreply.github.com>
2020-11-24 16:51:16 +0200

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -15,7 +15,7 @@
 		
 			@yield('content')
 			    <nav class="navbar fixed-bottom navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">&copy;2020</a>
+  <a class="navbar-brand" href="#">&copy;202021</a>
 </nav>
 
 	
